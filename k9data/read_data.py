67fdd31a8fb50883e9d44481b2fbdc75497b920b
Olga Bobrova <oleyka@gmail.com>
2016-09-06 02:34:07 -0700

    combine all sources of HD results

--- k9data/read_data.py
+++ k9data/read_data.py
@@ -46,14 +46,33 @@ def read_all(breed_id):
     return records
 
 
-def main():
-    breed_id = os.environ["k9data_breed"] if "k9data_breed" in os.environ else "WO"
-
-    records = read_all(breed_id)
+def count_tests(records):
     print "Test counts:"
     print records.groupby(["Registry Code"])["Registry Code"].count().sort_values()
 
-    ''' # NOTE read them all
+
+def count_hd_results(records):
+    print "HD records stats:"
+    hd_results = records.loc[lambda df: df["Registry Code"] == "HD", :]
+    ghd_results = records.loc[lambda df: df["Registry Code"] == "GHD", :]
+    ohd_results = records.loc[lambda df: df["Registry Code"] == "OHD", :]
+    phd_results = records.loc[lambda df: df["Registry Code"] == "PHD", :]
+    hd_all_results = pd.concat([hd_results, ghd_results, ohd_results, phd_results])
+    print hd_all_results.groupby("Results")["Results"].count().sort_values()
+    print "Total HD records:", hd_all_results["Results"].count()
+
+    '''  NOTE looking up broken records
+    hd_results = records.loc[lambda df: df["Registry Code"] == "HD", :].groupby(["Results"])["Results"]
+    print hd_results.loc[lambda df: df["Results"] == "12/11/2001", :] #.count()
+    '''
+
+
+def main():
+    breed_id = os.environ["k9data_breed"] if "k9data_breed" in os.environ else "WO"
+    records = read_all(breed_id)
+    count_hd_results(records)
+
+    '''  NOTE read them all
     codes = set()
     for breed_id in breeds:
         records = read_all(breed_id)
